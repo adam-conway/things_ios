a1ac50ad2d98cad5ea56bfef001245e7283fdd45
Adam Conway <adam.n.conway@gmail.com>
2023-01-03 16:23:18 -0800

    Initial Commit

---
+++ things_ios/things_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  things_iosApp.swift
+//  things_ios
+//
+//  Created by Adam Conway on 1/3/23.
+//
+
+import SwiftUI
+
+@main
+struct things_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
