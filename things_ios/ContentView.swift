f5416c06283ddc645035ac1ea0c87e8953d8cd0a
Adam Conway <adam.n.conway@gmail.com>
2023-01-04 16:38:59 -0800

    Messy initial implementation

--- things_ios/ContentView.swift
+++ things_ios/ContentView.swift
@@ -8,14 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var passwordConfirmation: String = ""
+    @State private var isSubmittingForm: Bool = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Form {
+                Section(header: Text("Credentials")) {
+                    TextField("Email", text: $username) // Add validation for being proper email
+                    SecureField("Password", text: $password) // Add validation for passwords matching, security toggle
+                    SecureField("Password", text: $passwordConfirmation) // Add validation for passwords matching, security toggle
+                }
+                Button("Sign up") {
+                    isSubmittingForm = true
+                    
+                    // API functionality should be extracted from the view
+                    var request = URLRequest(url: URL(string: "http://localhost:3000/users")!) // Replace this with environment variable
+                    request.httpMethod = "POST"
+                    let json: [String: Any] = ["user": ["email": username,"password": password]]
+                    
+                    if let jsonData = try? JSONSerialization.data(withJSONObject: json, options: .prettyPrinted) {
+                        request.httpBody = jsonData
+                    }
+                    request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+                    request.addValue("application/json", forHTTPHeaderField: "Accept")
+                    
+                    let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                        isSubmittingForm = false
+                        guard let data = data, let response = response, error == nil else {
+                            print("Something went wrong: error: \(error?.localizedDescription ?? "unkown error")")
+                            return
+                        }
+                        
+                        print(response)
+                        print(String(decoding: data, as: UTF8.self))
+                    }
+                    
+                    task.resume()
+                    // Add something to indicate activity happening
+                }.disabled(isSubmittingForm)
+            }
         }
-        .padding()
     }
 }
 
